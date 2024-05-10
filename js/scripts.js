d9ab182296e66524b7c9fb48ac0e496ab774f191
Siphosakhe Mahlangu <61363539+sipho12345@users.noreply.github.com>
2024-05-11 00:32:10 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -248,11 +248,6 @@ $(function() {
 
 });
 
-var siteWidth = 1280;
-var scale = screen.width /siteWidth;
-
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
-
 
 
 
